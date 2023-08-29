9c4cfbd15e3627df6d65f21e6789d988b3cd17fd
Anastasiia Prokhorova <naumtasya@gmail.com>
2023-08-29 13:23:08 +0700

    Initial commit1

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        int ticketPrice = 20_610;
+        int oneBonusMile = 20;
 
+        int BonusMile = ticketPrice / oneBonusMile;
 
-        for (int i = 1; i <= 5; i++) {
-
-          
-            System.out.println("i = " + i);
-        }
+        System.out.println("Всего начислено бонусов: "  + BonusMile + " руб.");
     }
 }
